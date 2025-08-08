a66a09e11002c6f51f2ad75c2a41adfd9fc66029
caito <caitocd@gmail.com>
2025-08-08 17:50:59 -0300

    email service added

--- notification-service/src/main/java/com/ch/notificationservice/consumers/HiighMsgConsumer.java
+++ notification-service/src/main/java/com/ch/notificationservice/consumers/HiighMsgConsumer.java
@@ -1,8 +1,13 @@
 package com.ch.notificationservice.consumers;
 
 import com.ch.core.chcore.events.HighMsg;
+import com.ch.notificationservice.services.contracts.MailSender;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Consumer for high priority messages.
@@ -13,7 +18,9 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
+@RequiredArgsConstructor
 public class HiighMsgConsumer {
+    private final MailSender mailSender;
 
     /**
      * Listens to the "highTopic" Kafka topic and processes HighMsg messages.
@@ -22,9 +29,22 @@ public class HiighMsgConsumer {
      */
     @KafkaListener(topics = "highTopic", groupId = "high-user-group")
     public void handleUserRegistry(HighMsg msg){
-        System.out.println("Received message: " + msg.getEmail() + " " + msg.getUsername() + " " + msg.getValidationToken());
+        System.out.println("Received message: " + msg.getEmail() + " " + msg.getUsername() + " "
+                + msg.getValidationToken());
+        Map<String, String> data = new HashMap<>();
+        data.put("name", msg.getUsername());
+        data.put("token", msg.getValidationToken());
+        mailSender.sendEmailWithTemplate(new String[]{msg.getEmail()},
+                "Account Activation - No Reply",
+                "templates/account-activation.html",
+                data);
     }
 
+    /**
+     * Listens to the "enableUserTopic" Kafka topic and processes incoming messages.
+     *
+     * @param msg the message received from the Kafka topic
+     */
     @KafkaListener(topics = "enableUserTopic", groupId = "high-enable-group")
     public void handleEnableUser(String msg){
         System.out.println("Received message: " + msg);
